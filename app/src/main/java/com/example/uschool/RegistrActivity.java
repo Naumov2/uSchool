d685429d975bb997724436cafa5b55859d01b5ac
Iiya <ilyanaumov387@gmail.com>
2024-04-01 19:33:56 +0400

    Главное меню 2.0

---
+++ app/src/main/java/com/example/uschool/RegistrActivity.java
@@ -0,0 +1,20 @@
+package com.example.uschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+public class RegistrActivity extends AppCompatActivity {
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registr);
+        spinner = findViewById(R.id.function);
+    }
+}
